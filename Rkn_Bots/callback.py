854b8badde6958fc74721c3dcf98607f07cfbb37
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-10-26 10:31:13 +0530

    Update callback.py

--- Rkn_Bots/callback.py
+++ Rkn_Bots/callback.py
@@ -3,17 +3,4 @@ from pyrogram.types import *
 @Client.on_callback_query()
 async def cb_handler(bot, query: CallbackQuery):
      if query.data == "about":
-        buttons1 = [[
-            InlineKeyboardButton('🔙 back', callback_data='start'),
-            InlineKeyboardButton('🔒 Cʟᴏsᴇ', callback_data='close_data')
-        ]]
-        await bot.edit_message_media(
-            query.message.chat.id, 
-            query.message.id, 
-        )
-        reply_markup = InlineKeyboardMarkup(buttons1)
-        await query.message.edit_text(
-            text="hii",
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
+        await query.message.edit_text(text="hii")
